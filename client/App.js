09985424794547859f3db94ce149f74326ef7db8
razananisathar <frazana.oa@gmail.com>
2021-05-02 02:23:12 +0530

    create shopping list and pantry list tab views

--- client/App.js
+++ client/App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import tailwind from 'tailwind-rn';
+import {Provider} from 'react-redux';
+import store from './store/store.js';
+import Header from './components/Header';
+import TabsView from './components/Tabs/Tabs';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Header />
+      </View>
+      <TabsView />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
   },
 });
---
+++ client/components/Tabs/Shopping/ShoppingList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button, Pressable} from 'react-native';
+import tailwind from 'tailwind-rn';
+
+export default function ShoppingListContainer() {
+  return (
+    <View>
+      <Text>Shopping List</Text>
+    </View>
+  );
+}
