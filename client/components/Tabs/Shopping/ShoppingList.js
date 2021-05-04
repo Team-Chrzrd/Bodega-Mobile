0089bdd0100f2e96e7b6b3350d841d44a06a8e04
Jason Lee <jasonmlee1020@gmail.com>
2021-05-03 19:36:28 -0700

    Shopping List UI Features

--- client/components/Tabs/Shopping/ShoppingList.js
+++ client/components/Tabs/Shopping/ShoppingList.js
@@ -3,7 +3,7 @@ import {StyleSheet, View, FlatList, Text} from 'react-native';
 import tailwind from 'tailwind-rn';
 import {useSelector, useDispatch} from 'react-redux';
 import {getShoppingItems} from '../../../store/actions/shoppingActions';
-// import {ShoppingItem} from '../../Tabs/Shopping/ShoppingItem';
+import ShoppingItem from '../../Tabs/Shopping/ShoppingItem';
 
 export default function ShoppingListContainer() {
   const shoppingItems = useSelector(state => state.shopping.shoppingList);
@@ -13,22 +13,17 @@ export default function ShoppingListContainer() {
     dispatch(getShoppingItems());
   }, []);
 
+  const renderItem = ({ item }) => (
+    <ShoppingItem itemDetails={item} />
+  );
+
   return (
     <View>
       <FlatList
-        data={shoppingItems}
+        data={shoppingItems.slice(0,10)}
         keyExtractor={(item, _id) => _id.toString()}
-        renderItem={item => (
-          <Text style={tailwind('text-white')}>{item.item.item_name}</Text>
-        )}
+        renderItem={renderItem}
       />
-      {/* <ul className="divide-y divide-gray-200">
-        {shoppingItems
-          .sort((a, b) => sortItem(a, b))
-          .map((item, i) => {
-            return <ShoppingItem key={`item${i}`} newItem={item} />;
-          })}
-      </ul> */}
     </View>
   );
 }
