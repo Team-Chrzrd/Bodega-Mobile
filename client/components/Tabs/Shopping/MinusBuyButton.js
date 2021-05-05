634ffabd48a3c09a0641de7b97374bd7bd1f0243
razananisathar <frazana.oa@gmail.com>
2021-05-05 06:07:40 +0530

    Add shopping list update and delete button

---
+++ client/components/Tabs/Shopping/MinusBuyButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Pressable} from 'react-native';
+import tailwind from 'tailwind-rn';
+// import {useDispatch} from 'react-redux';
+// import {minusBy} from '../../../store/actions/shoppingActions';
+
+//Decrements required stock QTY
+export default function MinusBuyButton({id}) {
+  //   const dispatch = useDispatch();
+
+  return <View></View>;
+  //   return (
+  //     <View style={tailwind('relative z-0 inline-flex shadow-md rounded-md')}>
+  //       <Pressable
+  //         id="minus-btn"
+  //         style={tailwind(
+  //           'relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500',
+  //         )}>
+  //         -
+  //       </Pressable>
+  //     </View>
+  //   );
+}
